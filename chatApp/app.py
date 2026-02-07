9bb59fa8f5aaac4b6750b7b24cbbe323cefe1aaf
TULASI VAMSI KRISHNA SAI <99528749+Tulasi999Vamsi@users.noreply.github.com>
2026-02-07 15:43:04 +0530

    Update app.py

--- chatApp/app.py
+++ chatApp/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request,render_template,jsonify
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_cors import CORS
-from models import db, User, Message
+from chatApp.models import db, User, Message
 from datetime import datetime
 
 # ------------------------------
